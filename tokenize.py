e0d1f9b4df0933275a76f03536ed4e69d45a7507
pchizhov <chizhov.pasha@yandex.ru>
2024-05-29 13:57:31 +0200

    Reorder logging commands

--- tokenize.py
+++ tokenize.py
@@ -8,7 +8,6 @@ from functools import lru_cache
 
 
 import logging
-logger = logging.getLogger(__name__)
 
 
 class BPEModel:
@@ -101,13 +100,15 @@ class BPEModel:
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
     parser = argparse.ArgumentParser()
     parser.add_argument('--bpe_model', type=str, required=True, help='Path to the BPE model.')
     parser.add_argument('--input_file', type=str, required=True, help='Path to the input file.')
     parser.add_argument('--output_file', type=str, required=True, help='Path to the output file.')
     parser.add_argument('--return_type', type=str, default='str', help='Return type: str or int.')
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
 
     model = BPEModel(args.bpe_model)
     model.encode_file(args.input_file, args.output_file, args.return_type)
