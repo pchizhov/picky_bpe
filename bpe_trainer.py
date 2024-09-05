55f3cd7d7a0ef37a269468e481c0bf7708478161
pchizhov <chizhovpd@gmail.com>
2024-09-05 17:23:28 +0200

    Reorder parameters

--- bpe_trainer.py
+++ bpe_trainer.py
@@ -294,16 +294,16 @@ class BPE:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_file', type=str, help='Path to the input file.')
-    parser.add_argument('model_file', type=str, help='Path to the output model file.')
-    parser.add_argument('--vocab_size', type=int, default=32768, help='Desired vocabulary size.')
-    parser.add_argument('--logging_step', type=int, default=200, help='Logging step.')
+    parser.add_argument('--input_file', type=str, help='Path to the input file.')
+    parser.add_argument('--model_file', type=str, help='Path to the output model file.')
+    parser.add_argument('--vocab_size', type=int, help='Desired vocabulary size.')
+    parser.add_argument('--threshold', type=float, help='Desired threshold.')
     parser.add_argument('--coverage', type=float, default=0.9999, help='Desired coverage.')
-    parser.add_argument('--threshold', type=float, default=0.9, help='Desired threshold.')
     parser.add_argument('--pad_id', type=int, default=0, help='ID of the padding token.')
     parser.add_argument('--unk_id', type=int, default=1, help='ID of the unknown token.')
     parser.add_argument('--bos_id', type=int, default=2, help='ID of the beginning-of-sequence token.')
     parser.add_argument('--eos_id', type=int, default=3, help='ID of the end-of-sequence token.')
+    parser.add_argument('--logging_step', type=int, default=200, help='Logging step.')
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.DEBUG)
